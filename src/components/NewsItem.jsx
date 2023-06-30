cd19b283184f5f0e10bd10591a2204d3b5df9740
SanketLakhani <sanketlakhani80@gmail.com>
2023-06-30 15:23:03 +0530

    add previous and next button to roam around the website and link to the News Api to get the news.

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -7,15 +7,23 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img
+            src={
+              !imageUrl
+                ? "https://www.gannett-cdn.com/presto/2023/06/27/PDTF/9c723c5d-d63e-4f01-83c1-490a39818f30-RG106413.jpg?auto=webp&crop=2399,1350,x0,y244&format=pjpg&width=1200"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}</p>
+            <p className="card-text">{description}...</p>
             <a
               href={newsUrl}
               rel="noreferrer"
               target="_blank"
-              className="btn btn-sm btn-primary"
+              className="btn btn-sm btn-dark"
             >
               Read More
             </a>
