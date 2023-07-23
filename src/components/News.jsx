a506fc87966a232806c546322dd867e337b3da56
SanketLakhani <sanketlakhani80@gmail.com>
2023-07-23 15:01:42 +0530

    adding react-router-dom to categories and some styling to newsItem.

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -99,14 +99,13 @@ export class News extends Component {
               return (
                 <div className="col-md-4" key={element.url}>
                   <NewsItem
-                    title={element.title ? element.title.slice(0, 45) : ""}
-                    description={
-                      element.description
-                        ? element.description.slice(0, 88)
-                        : ""
-                    }
+                    title={element.title ? element.title : ""}
+                    description={element.description ? element.description : ""}
                     imageUrl={element.urlToImage}
                     newsUrl={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                    source={element.source.name}
                   />
                 </div>
               );
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -2,11 +2,18 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
 
     return (
       <div className="my-3">
         <div className="card">
+          <span
+            class="position-absolute top-0 translate-middle badge rounded-pill bg-danger"
+            style={{ left: "90%", zIndex: "1" }}
+          >
+            {source}
+          </span>
           <img
             src={
               !imageUrl
@@ -17,8 +24,14 @@ export class NewsItem extends Component {
             alt="..."
           />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {author ? author : "Unknown"} on{" "}
+                {new Date(date).toUTCString()}
+              </small>
+            </p>
             <a
               href={newsUrl}
               rel="noreferrer"
